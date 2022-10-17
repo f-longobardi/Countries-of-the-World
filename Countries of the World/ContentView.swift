243134e2aacd06d5ba82b1ee27e41a826e28d6ba
Francesco <s.f.longobardi@gmail.com>
2022-10-17 14:12:17 +0200

    Initial Commit

---
+++ Countries of the World/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Countries of the World
+//
+//  Created by Sabato Francesco Longobardi on 17/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countries of the World/Countries_of_the_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_of_the_WorldApp.swift
+//  Countries of the World
+//
+//  Created by Sabato Francesco Longobardi on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Countries_of_the_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
